642725697f65a0d724592c003772ef8eda3c9a18
eneukirchner <nr@bulme.at>
2017-11-29 23:41:07 +0100

    errors corrected

--- progress.c
+++ progress.c
@@ -1,28 +1,75 @@
+/*
+    Progress bar
+    Unix version
+*/
 #include <stdio.h>
 #include <locale.h>
-#include <unistd.h>
+#include <time.h>
+#include <signal.h>
+#include <stdlib.h>
 
-#define KA 9632
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+
+// UTF-8 Code
+#define RECTANGLE 9632
+
+// reset terminal colors at ctrl-c
+void  INThandler(int sig)
+{
+    printf(ANSI_COLOR_RESET "Finished\n");
+    exit(0);
+}
 
 int main()
 {
+    signal(SIGINT, INThandler);
 	setlocale(LC_ALL,"");
+    struct timespec ts = {0, 30000000L };
+    
 	int i;
-	int bar[10];
-	
-	// initialisiere bar mit Leerzeichen
-	for (i= 0; i < sizeof(bar) -1 ; i++)
+	int bar[50];
+	int size = sizeof(bar)/sizeof(int);
+    
+	// initialize bar with spaces
+	for (i = 0; i < size -1; i++)
 		bar[i] = ' ';
-	bar[sizeof(bar) - 1] = '\0';
-	
-	for (i = 0; i < sizeof(bar) - 1; i++) {
-			bar[i] = KA;
-			fflush(stdout);
-			printf("\r%ls", bar);
-			sleep(1);
-	}
+        
+    // terminate string
+    bar[size - 1] = '\0';
+
+	for(;;) {
+        for (i = 0; i < size - 1; i++) {
+                bar[i] = RECTANGLE;
+                fflush(stdout);
+                if (i < size/3)
+                    printf(ANSI_COLOR_GREEN "\r%ls", bar);
+                else if (i < size/3*2)
+                    printf(ANSI_COLOR_YELLOW "\r%ls", bar);
+                else
+                    printf(ANSI_COLOR_RED "\r%ls", bar);
+                nanosleep(&ts, NULL);
+        }
+        
+        for (i = size - 2; i >= 0; i--) {
+                bar[i] = ' ';
+                fflush(stdout);
+                if (i > size/3*2)
+                    printf(ANSI_COLOR_RED "\r%ls", bar);
+                else if (i > size/3)
+                    printf(ANSI_COLOR_YELLOW "\r%ls", bar);
+                else
+                    printf(ANSI_COLOR_GREEN "\r%ls", bar);
+                nanosleep(&ts, NULL);
+        }
+    }
 		
-	printf("\n");
 	return 0;
 }
 
